2a666a4a8934b5344dd7d228ec53c6b6186dbe77
Mauricio Gonzalez <h.gonzalez@globant.com>
2023-02-07 18:35:14 -0500

    Logging

--- src/brewery/brewery.controller.ts
+++ src/brewery/brewery.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Logger, Query } from '@nestjs/common';
 import { Brewery } from 'src/types/brewery.types';
 import { BreweryService } from './brewery.service';
 
 @Controller('brewery')
 export class BreweryController {
+  private readonly log: Logger = new Logger(BreweryController.name);
+
   constructor(private readonly service: BreweryService) {}
 
   @Get()
@@ -11,9 +13,16 @@ export class BreweryController {
     @Query('page') page: number,
     @Query('per_page') pageSize: number,
   ): Promise<Brewery[]> {
+    this.log.log(
+      `Recibiendo solicitud de breweries page: ${page} per_page: ${pageSize}`,
+    );
     const fixedPage = page && page >= 0 ? page : 0;
     const fixedPageSize = pageSize && pageSize > 0 ? pageSize : 10;
-
-    return this.service.getPage(fixedPage, fixedPageSize);
+    const result: Brewery[] = await this.service.getPage(
+      fixedPage,
+      fixedPageSize,
+    );
+    this.log.log(`Respondiendo breweries cantidad: ${result.length}`);
+    return result;
   }
 }
--- src/brewery/brewery.service.ts
+++ src/brewery/brewery.service.ts
@@ -1,16 +1,20 @@
 import { HttpService } from '@nestjs/axios';
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { Brewery } from 'src/types/brewery.types';
 import { AxiosResponse } from 'axios';
 
 @Injectable()
 export class BreweryService {
+  private readonly log: Logger = new Logger(BreweryService.name);
   private readonly baseUri = 'https://api.openbrewerydb.org/';
 
   constructor(private readonly httpService: HttpService) {}
 
   async getPage(page: number, pageSize: number): Promise<Brewery[]> {
     const url = `${this.baseUri}breweries?per_page=${pageSize}&page=${page}`;
+
+    this.log.debug(`Consultando breweries url: ${url}`);
+
     const response: AxiosResponse<any[]> = await this.httpService.axiosRef.get(
       url,
       {
@@ -21,6 +25,10 @@ export class BreweryService {
       },
     );
     if (response.status !== HttpStatus.OK) {
+      this.log.error(
+        `Respuesta desde Open Brewery DB con status diferente de 200`,
+        response,
+      );
       throw new HttpException(response.data, response.status);
     }
     const breweries: Brewery[] = response.data.map((res) => ({
